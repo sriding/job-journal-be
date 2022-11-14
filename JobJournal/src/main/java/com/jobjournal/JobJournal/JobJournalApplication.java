036c47f7eba5c9cc731c0db0571df3a2f090c0f6
sriding <ridingjr@gmail.com>
2022-11-14 14:30:38 -0800

    .

--- JobJournal/src/main/java/com/jobjournal/JobJournal/JobJournalApplication.java
+++ JobJournal/src/main/java/com/jobjournal/JobJournal/JobJournalApplication.java
@@ -1,12 +1,45 @@
 package com.jobjournal.JobJournal;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Authenticator;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.PasswordAuthentication;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class JobJournalApplication {
-
 	public static void main(String[] args) {
-		SpringApplication.run(JobJournalApplication.class, args);
+		try {
+			URL proxyUrl = new URL(System.getenv("QUOTAGUARDSTATIC_URL"));
+			String userInfo = proxyUrl.getUserInfo();
+			String user = userInfo.substring(0, userInfo.indexOf(':'));
+			String password = userInfo.substring(userInfo.indexOf(':') + 1);
+
+			URLConnection conn = null;
+			System.setProperty("http.proxyHost", proxyUrl.getHost());
+			System.setProperty("http.proxyPort", Integer.toString(proxyUrl.getPort()));
+
+			Authenticator.setDefault(new Authenticator() {
+				protected PasswordAuthentication getPasswordAuthentication() {
+					return new PasswordAuthentication(user, password.toCharArray());
+				}
+			});
+
+			URL url = new URL("http://ip.quotaguard.com");
+			conn = url.openConnection();
+			conn.connect();
+
+			SpringApplication.run(JobJournalApplication.class, args);
+		} catch (IOException ie) {
+			ie.printStackTrace();
+		}
 	}
 }
